af54b5b25ddcf749397e984c3939cefdfdeb8045
Cmrlj <slavc@email.com>
2022-03-06 10:08:00 +0100

    Initialize repository

---
+++ lib/slavcg_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "slavcg_palindrome/version"
+
+module SlavcgPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/slavcg_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SlavcgPalindrome
+  VERSION = "0.1.0"
+end
